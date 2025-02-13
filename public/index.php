c21f8985f74374951d4648d7a0114a4bd09d4d37
Matthew Setter <matthew@matthewsetter.com>
2025-02-13 11:55:03 +1000

    Update the application's configuration
    This change provides a core configuration for the application,
    registering a number of services that initialise the view/template
    layer. There may well have been simpler ways to do this, but it works
    for the time being.

--- public/index.php
+++ public/index.php
@@ -2,19 +2,51 @@
 
 declare(strict_types=1);
 
-use Laminas\Diactoros\Response\TextResponse;
+use App\Handler\ConvertMarkdownFileHandlerFactory;
+use App\Handler\ConvertMarkdownFileHandler;
 use Laminas\ServiceManager\ServiceManager;
 use Asgrim\MiniMezzio\AppFactory;
 use Mezzio\Router\FastRouteRouter;
 use Mezzio\Router\Middleware\DispatchMiddleware;
 use Mezzio\Router\Middleware\RouteMiddleware;
-use Psr\Http\Message\ResponseInterface;
-use Psr\Http\Message\ServerRequestInterface;
-use Psr\Http\Server\RequestHandlerInterface;
+use Mezzio\Template\TemplateRendererInterface;
+use Mezzio\Twig\TwigEnvironmentFactory;
+use Mezzio\Twig\TwigExtension;
+use Mezzio\Twig\TwigExtensionFactory;
+use Mezzio\Twig\TwigRenderer;
+use Mezzio\Twig\TwigRendererFactory;
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$container = new ServiceManager();
+$container = new ServiceManager([
+    'aliases' => [
+        TemplateRendererInterface::class => TwigRenderer::class,
+        'Twig_Environment'               => Environment::class,
+    ],
+    'factories' => [
+        ConvertMarkdownFileHandler::class => ConvertMarkdownFileHandlerFactory::class,
+        Environment::class   => TwigEnvironmentFactory::class,
+        TwigExtension::class => TwigExtensionFactory::class,
+        TwigRenderer::class  => TwigRendererFactory::class,
+    ],
+    'services' => [
+        'config' => [
+            'templates' => [
+                'extension' => 'html.twig',
+                'paths'     => [
+                    'app'    => [realpath(__DIR__) . '/../data/templates/app'],
+                    'error'  => [realpath(__DIR__) . '/../data/templates/error'],
+                    'layout' => [realpath(__DIR__) . '/../data/templates/layout'],
+                    FilesystemLoader::MAIN_NAMESPACE => [realpath(__DIR__) . '/../data/templates/app'],
+                ],
+            ],
+            'twig' => [],
+            'debug' => true,
+        ]
+    ]
+]);
 $router = new FastRouteRouter();
 $app = AppFactory::create($container, $router);
 $app->pipe(new RouteMiddleware($router));
